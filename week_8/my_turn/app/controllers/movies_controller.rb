347467db206a39a76e1398c7bfc03ce77bcd6bdc
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-05-29 22:11:27 -0700

    Added styling

--- week_8/my_turn/app/controllers/movies_controller.rb
+++ week_8/my_turn/app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+
 	def index
 		@movies = Movie.all
 	end
@@ -23,16 +24,30 @@ class MoviesController < ApplicationController
 	end
 
 	def now_playing
-		key = "2eb9dd8f85fb496f52538c58a314031c"
-		filter = "now_playing"
-		url = "http://api.themoviedb.org/3/movie/#{filter}?api_key=#{key}"
+		key = "2eb9dd8f85fb496f52538c58a314031c"  #api key
+		filter = "now_playing"  #search filter
+		url = "http://api.themoviedb.org/3/movie/#{filter}?api_key=#{key}"  #base url
 		response = HTTParty.get(url).parsed_response
 		@now_playing = response['results']
+		
+		#we loop here because we need to grab the imdb_id from each movie and add it to the
+		#original hash so that we can use it in the view
+		count = 0
+		@now_playing.each do |movie|
+			if count < 8
+				movie_id = movie['id']  #grab movie id to find the movies imdb id
+				imdb_url = "http://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{key}"  #movie detail url
+				temp = HTTParty.get(imdb_url)
+				@imdb_id = temp['imdb_id']  #grab movies imdb id
+				movie["imdb_id"] = @imdb_id  #add movies imdb id to the original hash
+				count += 1
+			end
+		end
 	end
 
 	private
 		def movie_params
-			params.require(:movie).permit(:title, :location, :user_id)
+			params.require(:movie).permit(:title, :location, :user_id)  #only allow these values to get added
 		end
 
 end
--- week_8/my_turn/app/controllers/welcome_controller.rb
+++ week_8/my_turn/app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
 		@movies = Movie.all
-		@d_user = User.find(1)
-		@j_user = User.find(2)
+		@users = User.all
 	end
 end
